40d9d30798a011ee55e51fb1fbd5e53068560251
unknown <codewithswastik@gmail.com>
2022-09-11 16:32:56 +0530

    Create conversions.py

---
+++ conversions.py
@@ -0,0 +1,18 @@
+import json
+with open("reactions.json","r") as f:
+    reactions = json.load(f)
+
+def convert(from_compound, to_compound):
+    for sub in reactions:        
+        for reag, prod in reactions[sub].items():
+            if prod == to_compound:
+                x = f" + {reag} --> {prod}"
+                if sub == from_compound:
+                    return sub + x
+                else:
+                    c = convert(from_compound, sub)
+                    if c:
+                        return c + x
+
+print(convert("methanol", "isocyanomethane"))
+    
