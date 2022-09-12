91b426aeec2781680bedb9bc96f68d75d7e6a564
unknown <codewithswastik@gmail.com>
2022-09-12 18:49:12 +0530

    Added Colors

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from conversions import generate_conversion_problem, convert
 
-print("\n\nClash of Chemists CLI\n\n")
+RED = "\033[1;31;40m"
+GREEN = "\033[1;32;40m"
+YELLOW = "\033[1;33;40m"
+WHITE = "\033[1;37;40m"
+ENDC = "\033[0m"
+
+print(RED + "\n\nClash of Chemists CLI\n\n" + ENDC)
 s, e = generate_conversion_problem()
-print(f"Q. Convert {s} to {e}")
+print(f"Q. Convert {GREEN + s + ENDC} to {GREEN + e + ENDC}")
 input("Press enter to see 1 possible solution.")
-print(convert(s,e))
+cs = convert(s,e).split(" ")
+for i,x in enumerate(cs):
+    if x in "+→":
+        print(WHITE + x + ENDC, end = ' ')
+    elif x in (s,e) or cs[i-1]=="→":
+        print(GREEN + x + ENDC, end = ' ')
+    else:
+        print(YELLOW + x + ENDC, end = ' ')
+
+print("\n\n")
