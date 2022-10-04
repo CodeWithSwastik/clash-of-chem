be670207201843ef0aa3f976cba0905356990f85
unknown <codewithswastik@gmail.com>
2022-10-04 14:29:06 +0530

    Add slug

--- web/frontend/src/routes/+page.svelte
+++ web/frontend/src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+
 	let username: string;
 
-	function start() {
-
-	}
-
+	let start = () => {
+		goto('/clash');
+	};
 </script>
 
 <svelte:head>
@@ -16,20 +17,30 @@
 	<div class="flex items-center justify-center h-screen">
 		<div class="bg-gray-400 h-[50%] w-[50%] rounded shadow-xl p-10">
 			<div class="flex">
-				<input type="text" bind:value={username} placeholder="Enter your name" class="p-2 mr-2 rounded w-full">
-				<button on:click={start} class="bg-green-300 py-2 px-3 font-bold">Start</button>
+				<input
+					type="text"
+					bind:value={username}
+					placeholder="Enter your name"
+					class="p-2 mr-2 rounded w-full"
+				/>
+				<button on:click={start} class="bg-green-300 py-2 px-3 font-bold">Create Lobby</button>
 			</div>
 			<div>
 				<div class="my-10 text-2xl">Instructions to play</div>
 				<div>
 					<ul class="list-disc">
-						<li class="my-2">Lorem  amet consectetur adipisicing elit. Similique Dignissimos, nesciunt?</li>
-						<li class="my-2">Lorem  amet consectetur adipisicing elit. Similique Dignissimos, nesciunt?</li>
-						<li class="my-2">Lorem  amet consectetur adipisicing elit. Similique Dignissimos, nesciunt?</li>
+						<li class="my-2">
+							Lorem amet consectetur adipisicing elit. Similique Dignissimos, nesciunt?
+						</li>
+						<li class="my-2">
+							Lorem amet consectetur adipisicing elit. Similique Dignissimos, nesciunt?
+						</li>
+						<li class="my-2">
+							Lorem amet consectetur adipisicing elit. Similique Dignissimos, nesciunt?
+						</li>
 					</ul>
 				</div>
 			</div>
 		</div>
-	  </div>
+	</div>
 </section>
-
---
+++ web/frontend/src/routes/clash/[slug]/+page.svelte
@@ -0,0 +1,4 @@
+<script>
+	export let data;
+	console.log(data.id);
+</script>
