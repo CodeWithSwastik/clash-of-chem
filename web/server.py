e1114e3271b4fabe0c6d2cafa2c7f06dfaf99441
unknown <codewithswastik@gmail.com>
2022-10-04 18:13:19 +0530

    Add create_lobby

--- web/server.py
+++ web/server.py
@@ -1,13 +1,23 @@
-from ..cli import conversions as conv
+import uuid
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-api = FastAPI(title="Clash of Chemists Backend API")
-api.add_middleware(CORSMiddleware, allow_origins=["*"])
+app = FastAPI(title="Clash of Chemists Backend API")
+app.add_middleware(CORSMiddleware, allow_origins=["*"])
 
+lobbies = {}
 
 def substrate_image(substrate_name):
     """
     Generate image url for a given substrate.
     """
     return f"https://opsin.ch.cam.ac.uk/opsin/{substrate_name.strip()}.png"
+
+
+@app.get("/api/create_lobby")
+def create_lobby():
+    lobby_id = uuid.uuid4().hex
+    data = {"id": lobby_id, "players": []}
+    lobbies[lobby_id] = data
+    return lobby_id
+
