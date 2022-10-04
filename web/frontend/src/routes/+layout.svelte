449b1c579329e6b056ca8830164d09ab593993bb
Lonely-code-cube <lonely-code-cube@github.com>
2022-10-04 13:45:10 +0530

    Navbar and dark bg

--- web/frontend/src/routes/+layout.svelte
+++ web/frontend/src/routes/+layout.svelte
@@ -2,8 +2,14 @@
   	import "../app.css";
 </script>
 
-<div class="app">
+<div class="app bg-dark">
+	<nav class="flex p-2 absolute">
+		<a href="/" class="px-3 py-1 bg-gray-200 rounded-full">Home</a>
+	</nav>
 	<main>
 		<slot />
 	</main>
 </div>
+
+<style>
+</style>
