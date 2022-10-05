71ba6c7f6b4dfaf8120d944fab2ade029ccad412
unknown <codewithswastik@gmail.com>
2022-10-05 12:10:53 +0530

    Add leave button

--- web/frontend/src/routes/lobby/[id]/+page.svelte
+++ web/frontend/src/routes/lobby/[id]/+page.svelte
@@ -2,8 +2,12 @@
     import SmallCard from '$lib/components/SmallCard.svelte';
 	import { onMount } from 'svelte';
     import io from "socket.io-client";
+	import { goto } from '$app/navigation';
 
     export let data: {lobby: Lobby};
+
+    let start: Function, leave: Function;
+    let isOwner: boolean = true;
 
     $:players = [];
 
@@ -17,13 +21,25 @@
             }
         });
         socket.on("connect", () => console.log("connected"));
-        socket.on("room_details", (d) => {players = d.data});
+        socket.on("room_details", (d) => {
+            players = d.data;
+            isOwner = (players.length) == 1;
+        });
 
         socket.on("user_join", (d) => {
             if (d.data != username) {
                 players = [...players, d.data];
             }
         });
+
+        start = () => {
+            clearInterval(countdown);
+        };
+
+        leave = () => {
+            socket.disconnect();
+            goto('/');
+        }
 
     });
 
@@ -42,16 +58,12 @@
             start();
         }
     }, 1000);
-
-    let start = () => {
-        clearInterval(countdown);
-    };
 </script>
 
 <section>
     <div id="countdown" class="text-7xl text-gray-300 text-center my-5">{minutesLeft}:{secondsLeft<10?'0'+secondsLeft.toString():secondsLeft}</div>
     <div class="flex justify-around my-5">
-        <button on:click={start} class="p-2 bg-green-400 hover:bg-green-500 rounded">Start Clash</button>
+        <button on:click={isOwner? start : leave} class="p-2 bg-green-400 hover:bg-green-500 rounded">{isOwner? "Start": "Leave"} Clash</button>
     </div>
     <div id="players" class="flex flex-wrap py-10 px-20 justify-around">
         {#each players as player}
