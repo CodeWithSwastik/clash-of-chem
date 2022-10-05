1b8a2c25389b38954c5ad86d9b9d8f77cfc55381
unknown <codewithswastik@gmail.com>
2022-10-05 15:07:42 +0530

    Use catppuccin theme

--- web/frontend/src/routes/+page.svelte
+++ web/frontend/src/routes/+page.svelte
@@ -16,7 +16,8 @@
 
 <section>
 	<div class="flex items-center justify-center h-screen">
-		<div class="bg-gray-400 h-[50%] w-[50%] rounded shadow-xl p-10">
+		<div class="bg-sky w-[40%] rounded shadow-xl p-10">
+
 			<div class="flex">
 				<input
 					type="text"
@@ -24,23 +25,9 @@
 					placeholder="Enter your name"
 					class="p-2 mr-2 rounded flex-grow"
 				/>
-				<button on:click={start} class="bg-green-300 hover:bg-green-400 py-2 px-3 font-bold rounded">Create Lobby</button>
+				<button on:click={start} class="bg-base hover:bg-red text-white py-2 px-3 font-bold rounded">Create Lobby</button>
 			</div>
 			<div>
-				<div class="my-10 text-2xl">Instructions to play</div>
-				<div>
-					<ul class="list-disc">
-						<li class="my-2">
-							Lorem amet consectetur adipisicing elit. Similique Dignissimos, nesciunt?
-						</li>
-						<li class="my-2">
-							Lorem amet consectetur adipisicing elit. Similique Dignissimos, nesciunt?
-						</li>
-						<li class="my-2">
-							Lorem amet consectetur adipisicing elit. Similique Dignissimos, nesciunt?
-						</li>
-					</ul>
-				</div>
 			</div>
 		</div>
 	</div>
