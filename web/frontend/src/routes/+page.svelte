2b6f09ea308b9b18bdd8a6a9268013bc6396dad0
unknown <codewithswastik@gmail.com>
2022-10-06 13:51:16 +0530

    Make minor changes to the homepage

--- web/frontend/src/routes/+page.svelte
+++ web/frontend/src/routes/+page.svelte
@@ -32,10 +32,10 @@
 				/>
 			</div>
 			<div>
-			<button on:click={start} class="bg-green hover:bg-mauve text-base hover:text-black py-2 px-3 mt-8 w-full font-bold rounded">Play!</button>
+			<button on:click={start} class="bg-green hover:bg-opacity-70 text-base hover:text-black py-2 px-3 mt-8 w-full font-bold rounded">Play!</button>
 				</div>
 			<div>
-				<button on:click={start} class="bg-blue hover:bg-lavender text-white hover:text-black py-2 px-3 mt-2 w-full rounded">Create Private Lobby</button>
+				<button on:click={start} class="bg-sapphire hover:bg-opacity-70 text-base hover:text-black py-2 px-3 mt-2 w-full rounded">Create Private Lobby</button>
 			</div>
 
 			<div>
