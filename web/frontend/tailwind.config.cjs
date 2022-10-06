13e8cf21d303fff8d351d60889ca7dc30bbea362
Lonely-code-cube <lonely-code-cube@github.com>
2022-10-06 18:20:36 +0530

    Lobby page

--- web/frontend/tailwind.config.cjs
+++ web/frontend/tailwind.config.cjs
@@ -3,12 +3,6 @@ module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	darkMode: true,
 	theme: {
-		extend: {
-			colors: {
-				dark: '#191816',
-				'fg-dark': '#22231f'
-			}
-		}
 	},
 	plugins: [require('@catppuccin/tailwindcss')]
 };
