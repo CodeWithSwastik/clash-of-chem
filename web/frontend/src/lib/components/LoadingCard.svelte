5b0774170cdffca6fbdfa69407654cf2e566853f
unknown <codewithswastik@gmail.com>
2022-10-06 15:11:41 +0530

    Update Loading Card

--- web/frontend/src/lib/components/LoadingCard.svelte
+++ web/frontend/src/lib/components/LoadingCard.svelte
@@ -8,7 +8,7 @@
         "Carbon atoms make up the backbone of many important molecules in your body - proteins, DNA, mRNA, sugars and fats.",
         "Carbon is the fourth most lavish element in the universe, fifteenth on earth and second in the human body, after oxygen.",
         "Ev­ery hy­dro­gen atom in your body is around 13.5 bil­lion years old",
-        "If you mix a liter of wa­ter in a liter of ethyl al­co­hol, you get around 1.9 liters of so­lu­tion. This is because intermolecular forces between alcohol and water are strong."
+        "If you mix a liter of wa­ter in a liter of ethyl al­co­hol, you only get around 1.9 liters of so­lu­tion. Magic!"
     ]
     let fact = "";
     onMount(()=> fact = facts[Math.floor(Math.random()*facts.length)]);
@@ -19,7 +19,7 @@
     <div class="bg-transparent text-text rounded w-[30%] shadow-2xl p-10">
             <div class="text-center text-6xl">
                 <!-- TODO: Replace with benzene-->
-                <i class="fa-solid fa-atom text-red animate-spin-slow" /> 
+                <i class="fa-solid fa-atom text-sky animate-spin-slow" /> 
 
             </div>
 
