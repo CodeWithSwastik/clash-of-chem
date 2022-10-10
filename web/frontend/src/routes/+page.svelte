7cd9c99ae2c6426ae4cc8290e7366b71d0defafb
flicko <flickerdroid211@gmail.com>
2022-10-10 15:45:41 +0530

    change margins

--- web/frontend/src/routes/+page.svelte
+++ web/frontend/src/routes/+page.svelte
@@ -47,7 +47,7 @@
 				/>
 			</div>
 
-			<div class="grid grid-cols-2 mt-8">
+			<div class="grid grid-cols-2 mt-10">
 				<input
 					type="text"
 					bind:value={lobby}
@@ -57,7 +57,7 @@
 				<button on:click={start} class="bg-green hover:bg-opacity-70 text-base hover:text-black py-2 px-3 font-bold rounded-xl">Play!</button>
 			</div>
 			<div>
-				<button on:click={start} class="bg-sapphire hover:bg-opacity-70 text-base hover:text-black py-2 px-3 mt-2 w-full font-bold rounded-xl">Create Private Lobby</button>
+				<button on:click={start} class="bg-sapphire hover:bg-opacity-70 text-base hover:text-black py-2 px-3 mt-5 w-full font-bold rounded-xl">Create Private Lobby</button>
 			</div>
 		</div>
 	</div>
