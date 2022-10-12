5613faffa3f727445d2cae82e48f4ff1cace88e0
unknown <codewithswastik@gmail.com>
2022-10-12 13:23:11 +0530

    Update og tags

--- web/frontend/src/routes/+layout.svelte
+++ web/frontend/src/routes/+layout.svelte
@@ -2,6 +2,15 @@
 	import '../app.css';
 </script>
 
+<svelte:head>
+	<meta property="og:title" content="Clash of Chem" />
+	<meta
+		property="og:description"
+		content="Clash of Chem is a multiplayer online ogranic chemistry game that you can play with your friends!"
+	/>
+    <meta name="theme-color" content="#1e1e2e" />
+</svelte:head>
+
 <div class="app">
 	<main>
 		<slot />
--- web/frontend/src/routes/+page.svelte
+++ web/frontend/src/routes/+page.svelte
@@ -24,7 +24,13 @@
 
 <svelte:head>
 	<title>Home</title>
-	<meta name="description" content="Clash of Chem" />
+	<meta property="og:title" content="Clash of Chem" />
+	<meta
+		property="og:description"
+		content="Clash of Chem is a multiplayer online ogranic chemistry game that you can play with your friends!"
+	/>
+    <meta name="theme-color" content="#1e1e2e" />
+
 	<style lang="scss">		
 		@import "../style.scss";
 
